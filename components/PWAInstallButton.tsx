da5770b348cf0f83c8f63d85d5fd80a712ac84f0
P.P.K.S Dilhara <blogspotchaka@gmail.com>
2026-03-20 14:41:14 +0530

    minor ui changes

--- components/PWAInstallButton.tsx
+++ components/PWAInstallButton.tsx
@@ -47,7 +47,7 @@ export default function PWAInstallButton() {
   return (
     <Button
       onClick={install}
-      className="fixed bottom-5 left-4 z-50 rounded-md bg-info px-4 py-2 text-info-foreground shadow-lg hover:bg-info/90 focus:outline-none focus:ring-2 focus:ring-info/50 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-info/100"
+      className="fixed bottom-5 left-4 z-50 rounded-md bg-info px-4 py-2 text-info-foreground shadow-lg hover:bg-info/90 focus:outline-none focus:ring-2 focus:ring-info/50 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-info/100 hover:cursor-pointer"
     >
       Install App
     </Button>
